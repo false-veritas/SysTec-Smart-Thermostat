9200583df6a92df6d380a4418be7f02b493ef50b
Vito C <69727622+false-veritas@users.noreply.github.com>
2021-06-21 11:15:59 -0400

    Add files via upload

---
+++ pwmled2_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/pwmled2.d
@@ -0,0 +1,54 @@
+# FIXED
+
+pwmled2.obj: ../pwmled2.c
+pwmled2.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/kernel/nortos/posix/unistd.h
+pwmled2.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/kernel/nortos/posix/sys/types.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+pwmled2.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+pwmled2.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/PWM.h
+pwmled2.obj: syscfg/ti_drivers_config.h
+pwmled2.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/DeviceFamily.h
+
+../pwmled2.c:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/kernel/nortos/posix/unistd.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/kernel/nortos/posix/sys/types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/PWM.h:
+
+syscfg/ti_drivers_config.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/DeviceFamily.h:
+
---
+++ pwmled2_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/syscfg/ti_drivers_config.d
@@ -0,0 +1,84 @@
+# FIXED
+
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.c
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/DeviceFamily.h
+syscfg/ti_drivers_config.obj: syscfg/ti_drivers_config.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/PWM.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/pwm/PWMTimerCC32XX.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/Power.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/utils/List.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/power/PowerCC32XX.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/inc/hw_types.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/rom.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/rom_map.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/rom_patch.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/pin.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/prcm.h
+syscfg/ti_drivers_config.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/Board.h
+
+syscfg/ti_drivers_config.c:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/DeviceFamily.h:
+
+syscfg/ti_drivers_config.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/PWM.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/pwm/PWMTimerCC32XX.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdbool.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/Power.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/utils/List.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/power/PowerCC32XX.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/inc/hw_types.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/rom.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/rom_map.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/rom_patch.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/pin.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/cc32xx/driverlib/prcm.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/Board.h:
+
