a09030fcfbb687f9b4c354e8c1028ba886dc2f5e
Vito C <69727622+false-veritas@users.noreply.github.com>
2021-06-21 11:16:40 -0400

    Add files via upload

---
+++ uartecho_CC3220S_LAUNCHXL_nortos_ccs/MCU+Image/uartecho.d
@@ -0,0 +1,51 @@
+# FIXED
+
+uartecho.obj: ../uartecho.c
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h
+uartecho.obj: E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h
+uartecho.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/GPIO.h
+uartecho.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/UART.h
+uartecho.obj: syscfg/ti_drivers_config.h
+uartecho.obj: E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/DeviceFamily.h
+
+../uartecho.c:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_ti_config.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/linkage.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/_stdint40.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/cdefs.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_types.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/machine/_stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/sys/_stdint.h:
+
+E:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.5.LTS/include/stddef.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/GPIO.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/drivers/UART.h:
+
+syscfg/ti_drivers_config.h:
+
+E:/ti/ccs1031/simplelink_cc32xx_sdk_5_10_00_02/source/ti/devices/DeviceFamily.h:
+
